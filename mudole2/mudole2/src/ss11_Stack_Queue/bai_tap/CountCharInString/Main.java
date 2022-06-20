9aff3114cf0fe2009a28eee5ae7e9f5545a9f02f
lduchieu512 <lduchieu512@gmail.com>
2022-06-20 18:10:37 +0700

    20/6/22

---
+++ mudole2/mudole2/src/ss11_Stack_Queue/bai_tap/CountCharInString/Main.java
@@ -0,0 +1,25 @@
+package ss11_Stack_Queue.bai_tap.CountCharInString;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) {
+        String stTest = "Hello Bro!";
+        TreeMap<Character,Integer> treeMap = new TreeMap<>();
+        Arrays.asList(stTest.toUpperCase().split("")).forEach(e->{
+            int count =1;
+            if (treeMap.containsKey(e.charAt(0))){
+                count+= treeMap.get(e.charAt(0));
+            }
+
+            treeMap.put(e.charAt(0),count);
+        });
+        for (Map.Entry<Character,Integer> s: treeMap.entrySet()
+             ) {
+            System.out.println(s);
+
+        }
+    }
+}
